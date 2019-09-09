eebe9213912e7bd71ac2f12f2097d8df8fd31da4
Pradoxzon <speerenboom@gmail.com>
2019-09-09 10:37:39 -0400

    Add a class for math calculations on floating point numbers.

---
+++ Pradoxzon.CommOps/Math/FMath.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * FMath.cs
+ * 
+ * Copyright (c) 2019 Pradoxzon Dev
+ * 
+ * Author: Shawn Peerenboom (Pradoxzon)
+ * 
+ * This class implements methods for performing calculations
+ * on floating point numbers.
+ */
+
+namespace Pradoxzon.CommOps.Math
+{
+    using System;
+
+
+    /**
+     * <summary>Provides methods for performing calculations
+     * on floating point numbers.</summary>
+     */
+    public static class FMath
+    {
+
+    }
+}
