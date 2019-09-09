71972947ea53d1ca36142f0a20ec0bf852574d05
Pradoxzon <speerenboom@gmail.com>
2019-09-09 11:28:01 -0400

    Added some functions for float operations. Need to finish implementing the Round function.

--- Pradoxzon.CommOps/Math/FMath.cs
+++ Pradoxzon.CommOps/Math/FMath.cs
@@ -20,6 +20,69 @@ namespace Pradoxzon.CommOps.Math
      */
     public static class FMath
     {
+        #region Equals
+        /**
+         * <summary>Checks if two floating point numbers are equal
+         * with a tollerance of 0.000000001</summary>
+         * <param name="left">The first float to compare</param>
+         * <param name="right">The second float to compare</param>
+         * <returns>True if the floats are equal</returns>
+         */
+        public static bool Equals(float left, float right)
+        {
+            return Math.Abs(left - right) < 0.000000001f;
+        }
 
+        /**
+         * <summary>Checks if two floating point numbers are equal
+         * with a given tollerance</summary>
+         * <param name="left">The first float to compare</param>
+         * <param name="right">The second float to compare</param>
+         * <param name="tollerance">The tollerance to use when comparing</param>
+         * <returns>True if the floats are equal</returns>
+         */
+        public static bool Equals(float left, float right, float tollerance)
+        {
+            return Math.Abs(left - right) < tollerance;
+        }
+        #endregion
+
+
+        /**
+         * <summary>Gets the square root of a number</summary>
+         * <param name="num">The number to get the square root of</param>
+         * <returns>The square root</returns>
+         */
+        public static float Sqrt(float num)
+        {
+            return (float)Math.Sqrt(num);
+        }
+
+
+        #region Round
+        /**
+         * <summary>Rounds a floating point number to the nearest integer value</summary>
+         * <param name="num">The number to round</param>
+         * <returns>The rounded number</returns>
+         */
+        public static float Round(float num)
+        {
+            return (float)Math.Round(num);
+        }
+
+        /**
+         * <summary>Rounds a floating point number to the specified number of digits</summary>
+         * <param name="num">The number to round</param>
+         * <param name="decimals">The number of decimals to round to</param>
+         * <returns>The rounded number</returns>
+         */
+        public static float Round(float num, int decimals)
+        {
+            // TODO: Can this be simplified?
+            throw new NotImplementedException($"This function needs to be checked for functionality.");
+            double dnum = Convert.ToDouble(Convert.ToDecimal(num));
+            return (float)Math.Round(num, decimals);
+        }
+        #endregion
     }
 }
