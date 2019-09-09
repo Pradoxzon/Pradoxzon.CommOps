551e19173f4071bdbc94b04b9f7dcce271f8be02
Pradoxzon <speerenboom@gmail.com>
2019-09-09 12:27:22 -0400

    Added a header to the file.

--- Pradoxzon.CommOps.Testing/Math/FMathTest.cs
+++ Pradoxzon.CommOps.Testing/Math/FMathTest.cs
@@ -1,4 +1,13 @@
-﻿
+﻿/**
+ * FMathTest.cs
+ * 
+ * Copyright (c) 2019 Pradoxzon Dev
+ * 
+ * Author: Shawn Peerenbom (Pradoxzon)
+ * 
+ * Tests for the FMath class.
+ * See Pradoxzon.CommOps.Math.FMath for the class implentation.
+ */
 
 namespace Pradoxzon.CommOps.Testing.Math
 {
