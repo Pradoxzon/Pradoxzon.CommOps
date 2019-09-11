20ac7b5f18bb008c9515122f4b527e911d2d09c6
Pradoxzon <speerenboom@gmail.com>
2019-09-11 14:48:24 -0400

    Added a class for woking with bytes and bits.

---
+++ Pradoxzon.CommOps/Math/BitMath.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * BitMath.cs
+ * 
+ * Copyright (c) 2019 Pradoxzon Dev
+ * 
+ * Author: Shawn Peerenboom (Pradoxzon)
+ * 
+ * This class contains some constants and methods for
+ * working with bytes and for treating numbers as bits.
+ */
+
+namespace Pradoxzon.CommOps.Math
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    /**
+     * <summary>Provides methods and constants for working
+     * with bytes and for treating numbers as bits.</summary>
+     */
+    public static class BitMath
+    {
+        
+    }
+}
