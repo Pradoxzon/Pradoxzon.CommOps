3da00e9d40d1e54affd67b59ab969efb1467438a
Pradoxzon <speerenboom@gmail.com>
2019-09-12 20:12:35 -0400

    Started adding tests for BitMath.

---
+++ Pradoxzon.CommOps.Testing/Math/BitMathTest.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace Pradoxzon.CommOps.Testing.Math
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using static CommOps.Math.BitMath;
+
+
+    [TestClass]
+    public class BitMathTest
+    {
+        [TestMethod]
+        public void TestConstants()
+        {
+            // Sanity check, try and catch if one is ever changed for any reason?
+            Assert.IsTrue(NumBytes16Bits == 2,
+                $"There are 2 bytes in a 16-bit value, not {NumBytes16Bits} bytes.");
+
+            Assert.IsTrue(NumBytes32Bits == 4,
+                $"There are 4 bytes in a 32-bit value, not {NumBytes32Bits} bytes.");
+
+            Assert.IsTrue(NumBytes64Bits == 8,
+                $"There are 8 bytes in a 64-bit value, not {NumBytes64Bits} bytes.");
+
+            Assert.IsTrue(Bits16 == 16,
+                $"There are 16 bits in a 16-bit value, not {Bits16} bits.");
+
+            Assert.IsTrue(Bits32 == 32,
+                $"There are 32 bits in a 32-bit value, not {Bits32} bits.");
+
+            Assert.IsTrue(Bits64 == 64,
+                $"There are 64 bits in a 64-bit value, not {Bits64} bits.");
+        }
+    }
+}
