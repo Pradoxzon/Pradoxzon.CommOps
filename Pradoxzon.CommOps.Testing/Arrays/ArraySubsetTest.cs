bbb8aa18b25fab8cb46e397f7fe3eb74e62ead33
Pradoxzon <speerenboom@gmail.com>
2019-09-14 17:05:17 -0400

    Added a test class for the ArraySubset class.

---
+++ Pradoxzon.CommOps.Testing/Arrays/ArraySubsetTest.cs
@@ -0,0 +1,26 @@
+﻿/**
+ * ArraySubsetTest.cs
+ * 
+ * Copyright (c) 2019 Pradoxzon Dev
+ * 
+ * Author: Shawn Peerenboom (Pradoxzon)
+ * 
+ * Tests for the ArraySubset class.
+ * See Pradoxzon.CommOps.Arrays.ArraySubset for the class implementation.
+ */
+
+namespace Pradoxzon.CommOps.Testing.Arrays
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+    [TestClass]
+    public class ArraySubsetTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
