42df3b308cf20851321a2f571e20277b144db2f6
Pradoxzon <speerenboom@gmail.com>
2019-09-16 09:34:41 -0400

    Added a class for converting byte arrays to other types.

---
+++ Pradoxzon.CommOps/Arrays/FromBytes.cs
@@ -0,0 +1,41 @@
+﻿/**
+ * FromBytes.cs
+ * 
+ * Copyright (c) 2019 Pradoxzon Dev
+ * 
+ * Author: Shawn Peerenboom (Pradoxzon)
+ * 
+ * This class implements extensions to byte arrays that
+ * convert the arrays to other types.
+ */
+
+namespace Pradoxzon.CommOps.Arrays
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    /**
+     * <summary>Implements extensions to byte arrays that
+     * convert the arrays to other types.</summary>
+     */
+    public static class FromBytes
+    {
+        // Get string LE and BE
+
+        // Get List<string> LE and BE
+
+        // Get sbyte LE and BE
+
+        // Get byte LE and BE
+
+        // ... (integer types)
+
+        // Get float LE and BE
+
+        // Get double LE and BE
+    }
+}
