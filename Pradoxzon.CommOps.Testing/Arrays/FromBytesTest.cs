40cb599fcb50ac12dda87a94ebd79f41acc5fe04
Pradoxzon <speerenboom@gmail.com>
2019-09-18 09:57:43 -0400

    Added a class for testing FromBytes.

---
+++ Pradoxzon.CommOps.Testing/Arrays/FromBytesTest.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * FromBytesTest.cs
+ * 
+ * Copyright (c) 2019 Pradoxzon Dev
+ * 
+ * Author: Shawn Peerenboom (Pradoxzon)
+ * 
+ * Tests for the FromBytes class.
+ * See Pradoxzon.CommOps.Arrays.FromBytes for the class implementation.
+ */
+
+namespace Pradoxzon.CommOps.Testing.Arrays
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using static CommOps.Arrays.FromBytes;
+
+
+    [TestClass]
+    public class FromBytesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
