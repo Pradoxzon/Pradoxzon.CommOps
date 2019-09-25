ee5dd99be29b7a8e1d2fc9f5fa6478b0fa6613c4
Pradoxzon <speerenboom@gmail.com>
2019-09-25 09:51:21 -0400

    Finish testing for FromBytes.

--- Pradoxzon.CommOps.Testing/Arrays/FromBytesTest.cs
+++ Pradoxzon.CommOps.Testing/Arrays/FromBytesTest.cs
@@ -181,11 +181,296 @@ namespace Pradoxzon.CommOps.Testing.Arrays
             test = new byte[] { 0x11, 0x22 };
             Assert.ThrowsException<InvalidOperationException>(() => test.GetSbyte());
         }
+
+
+        [TestMethod]
+        public void TestGetShort()
+        {
+            // Max value - Big endian
+            byte[] test = { 0x7F, 0xFF };
+            var res = short.MaxValue;
+            Assert.IsTrue(res == test.GetShort(),
+                $"The values for test 1 did not match:\n" +
+                $"The array should equal {res}, not {test.GetShort()}");
+
+            // Normal value - Little endian
+            test = new byte[] { 0xBB, 0xAA };
+            res = -21829;
+            Assert.IsTrue(res == test.GetShort(true),
+                $"The values for test 2 did not match:\n" +
+                $"The array should equal {res}, not {test.GetShort(true)}");
+
+            // Min Value - Big endian
+            test = new byte[] { 0x80, 0x00 };
+            res = short.MinValue;
+            Assert.IsTrue(res == test.GetShort(),
+                $"The values for test 3 did not match:\n" +
+                $"The array should equal {res}, not {test.GetShort()}");
+
+            // Wrong size array
+            test = new byte[] { 0x11, 0x22, 0x33 };
+            Assert.ThrowsException<InvalidOperationException>(() => test.GetShort());
+        }
+
+
+        [TestMethod]
+        public void TestGetUshort()
+        {
+            // Max value - Big endian
+            byte[] test = { 0xFF, 0xFF };
+            var res = ushort.MaxValue;
+            Assert.IsTrue(res == test.GetUshort(),
+                $"The values for test 1 did not match:\n" +
+                $"The array should equal {res}, not {test.GetUshort()}");
+
+            // Normal value - Little endian
+            test = new byte[] { 0xAA, 0xBB };
+            res = 0xBBAA;
+            Assert.IsTrue(res == test.GetUshort(true),
+                $"The values for test 2 did not match:\n" +
+                $"The array should equal {res}, not {test.GetUshort(true)}");
+
+            // Min Value - Big endian
+            test = new byte[] { 0x00, 0x00 };
+            res = ushort.MinValue;
+            Assert.IsTrue(res == test.GetUshort(),
+                $"The values for test 3 did not match:\n" +
+                $"The array should equal {res}, not {test.GetUshort()}");
+
+            // Wrong size array
+            test = new byte[] { 0x11, 0x22, 0x33 };
+            Assert.ThrowsException<InvalidOperationException>(() => test.GetUshort());
+        }
+
+
+        [TestMethod]
+        public void TestGetInt()
+        {
+            // Max value - Big endian
+            byte[] test = { 0x7F, 0xFF, 0xFF, 0xFF };
+            var res = int.MaxValue;
+            Assert.IsTrue(res == test.GetInt(),
+                $"The values for test 1 did not match:\n" +
+                $"The array should equal {res}, not {test.GetInt()}");
+
+            // Normal value - Little endian
+            test = new byte[] { 0xDD, 0xCC, 0xBB, 0xAA };
+            res = -1430532899;
+            Assert.IsTrue(res == test.GetInt(true),
+                $"The values for test 2 did not match:\n" +
+                $"The array should equal {res}, not {test.GetInt(true)}");
+
+            // Min Value - Big endian
+            test = new byte[] { 0x80, 0x00, 0x00, 0x00 };
+            res = int.MinValue;
+            Assert.IsTrue(res == test.GetInt(),
+                $"The values for test 3 did not match:\n" +
+                $"The array should equal {res}, not {test.GetInt()}");
+
+            // Wrong size array
+            test = new byte[] { 0x11, 0x22, 0x33 };
+            Assert.ThrowsException<InvalidOperationException>(() => test.GetInt());
+        }
+
+
+        [TestMethod]
+        public void TestGetUint()
+        {
+            // Max value - Big endian
+            byte[] test = { 0xFF, 0xFF, 0xFF, 0xFF };
+            var res = uint.MaxValue;
+            Assert.IsTrue(res == test.GetUint(),
+                $"The values for test 1 did not match:\n" +
+                $"The array should equal {res}, not {test.GetUint()}");
+
+            // Normal value - Little endian
+            test = new byte[] { 0xAA, 0xBB, 0xCC, 0xDD };
+            res = 0xDDCCBBAA;
+            Assert.IsTrue(res == test.GetUint(true),
+                $"The values for test 2 did not match:\n" +
+                $"The array should equal {res}, not {test.GetUint(true)}");
+
+            // Min Value - Big endian
+            test = new byte[] { 0x00, 0x00, 0x00, 0x00 };
+            res = uint.MinValue;
+            Assert.IsTrue(res == test.GetUint(),
+                $"The values for test 3 did not match:\n" +
+                $"The array should equal {res}, not {test.GetUint()}");
+
+            // Wrong size array
+            test = new byte[] { 0x11, 0x22, 0x33 };
+            Assert.ThrowsException<InvalidOperationException>(() => test.GetUshort());
+        }
+
+
+        [TestMethod]
+        public void TestGetLong()
+        {
+            // Max value - Big endian
+            byte[] test = { 0x7F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+            var res = long.MaxValue;
+            Assert.IsTrue(res == test.GetLong(),
+                $"The values for test 1 did not match:\n" +
+                $"The array should equal {res}, not {test.GetLong()}");
+
+            // Normal value - Little endian
+            test = new byte[] { 0xDD, 0xCC, 0xBB, 0xAA, 0x44, 0x33, 0x22, 0x11 };
+            res = 0x11223344AABBCCDD;
+            Assert.IsTrue(res == test.GetLong(true),
+                $"The values for test 2 did not match:\n" +
+                $"The array should equal {res}, not {test.GetLong(true)}");
+
+            // Min Value - Big endian
+            test = new byte[] { 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            res = long.MinValue;
+            Assert.IsTrue(res == test.GetLong(),
+                $"The values for test 3 did not match:\n" +
+                $"The array should equal {res}, not {test.GetLong()}");
+
+            // Wrong size array
+            test = new byte[] { 0x11, 0x22, 0x33 };
+            Assert.ThrowsException<InvalidOperationException>(() => test.GetLong());
+        }
+
+
+        [TestMethod]
+        public void TestGetUlong()
+        {
+            // Max value - Big endian
+            byte[] test = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+            var res = ulong.MaxValue;
+            Assert.IsTrue(res == test.GetUlong(),
+                $"The values for test 1 did not match:\n" +
+                $"The array should equal {res}, not {test.GetUlong()}");
+
+            // Normal value - Little endian
+            test = new byte[] { 0xAA, 0xBB, 0xCC, 0xDD, 0x11, 0x22, 0x33, 0x44 };
+            res = 0x44332211DDCCBBAA;
+            Assert.IsTrue(res == test.GetUlong(true),
+                $"The values for test 2 did not match:\n" +
+                $"The array should equal {res}, not {test.GetUlong(true)}");
+
+            // Min Value - Big endian
+            test = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            res = ulong.MinValue;
+            Assert.IsTrue(res == test.GetUlong(),
+                $"The values for test 3 did not match:\n" +
+                $"The array should equal {res}, not {test.GetUlong()}");
+
+            // Wrong size array
+            test = new byte[] { 0x11, 0x22, 0x33 };
+            Assert.ThrowsException<InvalidOperationException>(() => test.GetUlong());
+        }
         #endregion
 
 
         #region Floats
+        [TestMethod]
+        public void TestGetFloat()
+        {
+            // Positive Infinity - Big endian
+            byte[] test = { 0x7F, 0x80, 0x00, 0x00 };
+            var res = float.PositiveInfinity;
+            Assert.IsTrue(float.IsPositiveInfinity(test.GetFloat()),
+                $"The values for test 1 did not match:\n" +
+                $"The array should equal {res}, not {test.GetFloat()}");
 
+            // Max value - Big endian
+            test = new byte[] { 0x7F, 0x7F, 0xFF, 0xFF };
+            res = float.MaxValue;
+            Assert.IsTrue(res == test.GetFloat(),
+                $"The values for test 2 did not match:\n" +
+                $"The array should equal {res}, not {test.GetFloat()}");
+
+            // Normal value - Little endian
+            test = new byte[] { 0x49, 0x46, 0x83, 0x05 };
+            res = 1.2345E-35f;
+            Assert.IsTrue(res == test.GetFloat(true),
+                $"The values for test 3 did not match:\n" +
+                $"The array should equal {res}, not {test.GetFloat(true)}");
+
+            // Epsilon - Big endian
+            test = new byte[] { 0x00, 0x00, 0x00, 0x01 };
+            res = float.Epsilon;
+            Assert.IsTrue(res == test.GetFloat(),
+                $"The values for test 4 did not match:\n" +
+                $"The array should equal {res}, not {test.GetFloat()}");
+
+            // Min value - Big endian
+            test = new byte[] { 0xFF, 0x7F, 0xFF, 0xFF };
+            res = float.MinValue;
+            Assert.IsTrue(res == test.GetFloat(),
+                $"The values for test 5 did not match:\n" +
+                $"The array should equal {res}, not {test.GetFloat()}");
+
+            // Negative Infinity - Big endian
+            test = new byte[] { 0xFF, 0x80, 0x00, 0x00 };
+            res = float.NegativeInfinity;
+            Assert.IsTrue(float.IsNegativeInfinity(test.GetFloat()),
+                $"The values for test 6 did not match:\n" +
+                $"The array should equal {res}, not {test.GetFloat()}");
+
+            // NaN - Big endian
+            test = new byte[] { 0xFF, 0xC0, 0x00, 0x00 };
+            res = float.NaN;
+            Assert.IsTrue(float.IsNaN(test.GetFloat()),
+                $"The values for test 7 did not match:\n" +
+                $"The array should equal {res}, not {test.GetFloat()}");
+        }
+
+
+        [TestMethod]
+        public void TestGetDouble()
+        {
+            // Positive Infinity - Big endian
+            byte[] test = { 0x7F, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            var res = double.PositiveInfinity;
+            Assert.IsTrue(double.IsPositiveInfinity(test.GetDouble()),
+                $"The values for test 1 did not match:\n" +
+                $"The array should equal {res}, not {test.GetDouble()}");
+
+            // Max value - Big endian
+            test = new byte[] { 0x7F, 0xEF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+            res = double.MaxValue;
+            Assert.IsTrue(res == test.GetDouble(),
+                $"The values for test 2 did not match:\n" +
+                $"The array should equal {res}, not {test.GetDouble()}");
+
+            // Normal value - Little endian
+            test = new byte[] { 0x00, 0x00, 0xE0, 0xFF, 0xFF, 0xFF, 0xEF, 0x41 };
+            res = 4.294967295E9;
+            Assert.IsTrue(res == test.GetDouble(true),
+                $"The values for test 3 did not match:\n" +
+                $"The array should equal {res}, not {test.GetDouble(true)}");
+
+            // Epsilon - Big endian
+            test = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
+            res = double.Epsilon;
+            Assert.IsTrue(res == test.GetDouble(),
+                $"The values for test 4 did not match:\n" +
+                $"The array should equal {res}, not {test.GetDouble()}");
+
+            // Min value - Big endian
+            test = new byte[] { 0xFF, 0xEF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+            res = double.MinValue;
+            Assert.IsTrue(res == test.GetDouble(),
+                $"The values for test 5 did not match:\n" +
+                $"The array should equal {res}, not {test.GetDouble()}");
+
+            // Negative Infinity - Big endian
+            test = new byte[] { 0xFF, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            res = double.NegativeInfinity;
+            Assert.IsTrue(double.IsNegativeInfinity(test.GetDouble()),
+                $"The values for test 6 did not match:\n" +
+                $"The array should equal {res}, not {test.GetDouble()}");
+
+            // NaN - Big endian
+            test = new byte[] { 0xFF, 0xF8, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            res = double.NaN;
+            Assert.IsTrue(double.IsNaN(test.GetDouble()),
+                $"The values for test 7 did not match:\n" +
+                $"The array should equal {res}, not {test.GetDouble()}");
+        }
         #endregion
     }
 }
