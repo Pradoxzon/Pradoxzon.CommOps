610627375336db61fc8bb72055aadad47fd8fcd1
Pradoxzon <speerenboom@gmail.com>
2019-09-30 15:06:03 -0400

    Updated the assembly info.

--- Pradoxzon.CommOps.Testing/Properties/AssemblyInfo.cs
+++ Pradoxzon.CommOps.Testing/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Pradoxzon.CommOps.Testing")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Shawn Peerenboom (Pradoxzon) 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
